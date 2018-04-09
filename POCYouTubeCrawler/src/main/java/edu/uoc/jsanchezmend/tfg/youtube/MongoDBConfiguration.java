be64d57f25a3e241d72793b9b8da09253c89446f
jsanchezmend <jsanchezmend@uoc.edu>
2018-04-09 22:31:14 +0200

    Package as a war and use Azure mongoDB connection

--- POCYouTubeCrawler/src/main/java/edu/uoc/jsanchezmend/tfg/youtube/MongoDBConfiguration.java
+++ POCYouTubeCrawler/src/main/java/edu/uoc/jsanchezmend/tfg/youtube/MongoDBConfiguration.java
@@ -4,18 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.mongodb.DB;
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
 import com.mongodb.MongoException;
 
 @Configuration
 public class MongoDBConfiguration {
 	
+	@SuppressWarnings({ "resource", "deprecation" })
 	@Bean
-	@SuppressWarnings("deprecation")
 	public DB mongoDB() throws MongoException {
 		System.out.println("Connecting to Mongo DB..");
-		final Mongo mongo = new Mongo("127.0.0.1");
-		return mongo.getDB("POCYoutubeCrawler");
+		
+		//final Mongo mongo = new Mongo("127.0.0.1");
+		//return mongo.getDB("POCYoutubeCrawler");
+		
+		final MongoClient mongoClient = new MongoClient(new MongoClientURI("mongodb://youtubecrawlertooldb:iC76QMhIQ9rQ03V3pbBra5xKcsG8NbI9rZxBXL0f2VDHBv5IC84f5rgcTybr4krxGI0cl7fkhLc9anZh223sxQ==@youtubecrawlertooldb.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"));
+		return mongoClient.getDB("POCYoutubeCrawler");
 	}
 
 }
--- POCYouTubeCrawler/src/main/java/edu/uoc/jsanchezmend/tfg/youtube/POCYoutubeCrawlerApp.java
+++ POCYouTubeCrawler/src/main/java/edu/uoc/jsanchezmend/tfg/youtube/POCYoutubeCrawlerApp.java
@@ -2,11 +2,18 @@ package edu.uoc.jsanchezmend.tfg.youtube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({MongoDBConfiguration.class, YouTubeConfiguration.class})
-public class POCYoutubeCrawlerApp {
+public class POCYoutubeCrawlerApp extends SpringBootServletInitializer {
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(POCYoutubeCrawlerApp.class);
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(POCYoutubeCrawlerApp.class, args);
