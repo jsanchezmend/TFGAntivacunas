066edc219db55df7cd5491ebf0bfc1fb8f34814b
jsanchezmend <jsanchezmend@uoc.edu>
2018-04-23 23:33:28 +0200

    Initial commit

---
+++ YouTubeCrawlerTool/README.md
@@ -0,0 +1,9 @@
+# YouTubeCrawlerTool 
+Trabajo final de grado estudio movimiento antivacunas
+
+-Start application:
+cd youtube-crawler-tool-war/
+mvn spring-boot:run
+
+- Homepage:
+http://localhost:8080/
---
+++ YouTubeCrawlerTool/youtube-crawler-tool-war/src/main/java/uoc/edu/jsanchezmend/tfg/ytct/YouTubeCrawlerToolApp.java
@@ -0,0 +1,32 @@
+package uoc.edu.jsanchezmend.tfg.ytct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
+
+/**
+ * 
+ * @author jsanchezmend
+ *
+ */
+@SpringBootApplication
+@EnableNeo4jRepositories("uoc.edu.jsanchezmend.tfg.ytct.data.repository")
+public class YouTubeCrawlerToolApp extends SpringBootServletInitializer {
+	
+	private final static Logger log = LoggerFactory.getLogger(YouTubeCrawlerToolApp.class);
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(YouTubeCrawlerToolApp.class);
+	}
+
+    public static void main(String[] args) {
+    	log.info("Starting YouTubeCrawlerToolApp...");
+        SpringApplication.run(YouTubeCrawlerToolApp.class, args);
+    }
+
+}
